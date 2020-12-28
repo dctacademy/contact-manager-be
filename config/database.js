7b778235622df62c33e749d3310d7ee0b861949b
harish <harishbn31@gmail.com>
2020-12-28 17:21:58 +0530

    database

--- config/database.js
+++ config/database.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
-mongoose.connect('mongodb://localhost:27017/blog-video', { useNewUrlParser: true})
+const CONNECTION_URI = process.env.MONGODB_URI || `mongodb://localhost:27017/contact-manager-be` 
+
+mongoose.connect(CONNECTION_URI, { useNewUrlParser: true})
     .then(function(){
         console.log('connected to db')
     })
